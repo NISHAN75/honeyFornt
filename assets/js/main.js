fb4681e6c5a6b815f255df3ff77c4bcf0aa58acc
nishan das <nishandas880@gmail.com>
2025-03-22 07:58:16 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+(function ($) {
+    $(document).ready(function () {
+
+	
+      
+
+
+ 
+
+
+
+
+
+
+        // OverlayScrollbars
+        const {
+            OverlayScrollbars,
+            ClickScrollPlugin
+        } = OverlayScrollbarsGlobal;
+        // Initialize the ClickScrollPlugin
+        OverlayScrollbars.plugin(ClickScrollPlugin);
+        $("body").each(function () {
+            OverlayScrollbars(this, {
+                scrollbars: {
+                    clickScroll: true,
+                    autoHide: "leave",
+                    dragScrolling: true,
+                    clickScrolling: true,
+                },
+                scrollBehavior: 'smooth',
+            });
+        });
+        // lenis
+        // Initialize a new Lenis instance for smooth scrolling
+        const lenis = new Lenis();
+
+        // Listen for the 'scroll' event and log the event data to the console
+        // lenis.on('scroll', (e) => {
+        //     console.log(e);
+        // });
+
+        // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+        lenis.on('scroll', ScrollTrigger.update);
+
+        // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+        // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+        gsap.ticker.add((time) => {
+            lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+        });
+
+        // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+        gsap.ticker.lagSmoothing(0);
+        // lenis
+
+
+
+
+        
+    });
+})(jQuery);
